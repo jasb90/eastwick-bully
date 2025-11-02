2cbbdeaf019854cccc4f5b393ce30777ea758c9f
Jasmine <jazb2151@gmail.com>
2025-11-02 17:03:48 -0600

    remove basePath for vercel audio

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // No basePath or assetPrefix on Vercel
-  // If you ever need static export for GitHub Pages again, we’ll toggle via an env var.
 };
 
 export default nextConfig;
-
-
-
