8b1763e0a9dcd91b454fbb10475ef77596eeb786
Jasmine <jazb2151@gmail.com>
2025-11-02 19:50:21 -0600

    fix domain basePath for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "",
+  basePath: "",
+};
+
 export default nextConfig;
